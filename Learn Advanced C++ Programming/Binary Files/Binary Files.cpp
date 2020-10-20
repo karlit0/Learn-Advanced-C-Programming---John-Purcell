625f457ce9667e618b615b55ec73147ff6de0110
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 14:02:47 +0100

    Files: Structs and Padding

---
+++ Learn Advanced C++ Programming/Binary Files/Binary Files.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#pragma pack(push, 1)
+
+struct Person
+{
+	char Name[50];
+	int Age;
+	double Weight;
+};
+
+#pragma pack(pop)
+
+int main()
+{
+	cout << sizeof(Person) << endl;
+
+   return 0;
+}
