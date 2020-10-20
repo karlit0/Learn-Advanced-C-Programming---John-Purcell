9367862cf37c92cfb44ca34512c8f20e75c0e286
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 13:27:47 +0100

    Files: Writing Text Files

---
+++ Learn Advanced C++ Programming/Writing Text Files/Writing Text Files.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    //ofstream OutFile;
+    fstream OutFile;
+
+    string OutFileName = "text.txt";
+    //string OutFileName = "text2.txt";
+    
+    //OutFile.open(OutFileName);
+    OutFile.open(OutFileName, ios::out);
+
+    if (OutFile.is_open())
+    {
+        OutFile << "Hello there" << endl;
+        OutFile << 123 << endl;
+        OutFile.close();
+    }
+    else
+    {
+        cout << "Could not create file: " << OutFileName << endl;
+    }
+
+    return 0;
+}
