7100f820ac09bba807c217d1bfa267a45fa63e47
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 14:20:21 +0100

    Files: Reading and Writing Binary Files

--- Learn Advanced C++ Programming/Binary Files/Binary Files.cpp
+++ Learn Advanced C++ Programming/Binary Files/Binary Files.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,14 +9,54 @@ struct Person
 {
 	char Name[50];
 	int Age;
-	double Weight;
+	double Height;
 };
 
 #pragma pack(pop)
 
 int main()
 {
-	cout << sizeof(Person) << endl;
+	Person Someone = {"Frodo", 220, 0.8};
 
-   return 0;
+	string FileName = "test.bin";
+
+	// Write binary file
+
+	ofstream OutputFile;
+
+	OutputFile.open(FileName, ios::binary);
+
+	if (OutputFile.is_open())
+	{
+		OutputFile.write(reinterpret_cast<char *>(&Someone), sizeof(Person));
+
+		OutputFile.close();
+	}
+	else
+	{
+		cout << "Could not create file " + FileName;
+	}
+
+	// Read binary file
+
+	Person SomeoneElse = {};
+
+	ifstream InputFile;
+
+	InputFile.open(FileName, ios::binary);
+
+	if (InputFile.is_open())
+	{
+		InputFile.read(reinterpret_cast<char *>(&SomeoneElse), sizeof(Person));
+
+		InputFile.close();
+	}
+	else
+	{
+		cout << "Could not read file " + FileName;
+	}
+
+	cout << SomeoneElse.Name << ", " << SomeoneElse.Age << ", " << SomeoneElse.Height << endl;
+
+	return 0;
 }
