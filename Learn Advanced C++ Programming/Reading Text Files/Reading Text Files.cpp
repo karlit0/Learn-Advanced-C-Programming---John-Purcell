88ed9310412bc6069baaf46d9c8c9f6922384d37
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 13:37:10 +0100

    Files: Reading Text Files

---
+++ Learn Advanced C++ Programming/Reading Text Files/Reading Text Files.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string InFileName = "test.txt";
+
+    ifstream InFile;
+
+    InFile.open(InFileName);
+
+    if (InFile.is_open())
+    {
+        string Line;
+
+        while(InFile)
+        {
+            getline(InFile, Line);
+            cout << Line << endl;
+        }
+        
+        InFile.close();
+    }
+    else
+    {
+        cout << "Cannot open file: " << InFileName << endl;
+    }
+
+    return 0;
+}
