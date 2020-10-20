72c3028a0bc8a91ba21a399223b45e37724cae16
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 12:29:54 +0100

    Exceptions: Exceptions Basics

--- Project/Project/Basic Exceptions.cpp
+++ Project/Project/Basic Exceptions.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void MightGoWrong()
+{
+	bool bError1 = false;
+	bool bError2 = true;
+
+	if (bError1)
+	{
+		throw "Something went wrong.";
+	}
+
+	if (bError2)
+	{
+		throw string("Something else went wrong.");
+	}
+}
+
+void UsesMightGoWrong()
+{
+	MightGoWrong();
+}
+
 int main()
 {
+	try
+	{
+		UsesMightGoWrong();
+	}
+	catch (int e)
+	{
+		cout << "Error code: " << e << endl;
+	}
+	catch(char const* e)
+	{
+		cout << "Error message: " << e << endl;
+	}
+	catch (string& e)
+	{
+		cout << "String Error Message: " << e << endl;
+	}
+	
+	cout << "Still running" << endl;
 
 	return 0;
 }
