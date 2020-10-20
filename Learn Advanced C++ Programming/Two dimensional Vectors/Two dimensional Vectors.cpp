ec229f07628b78b2c18357067ecf0f5db215bcc5
SUMO-DIGITAL\AStipanovic <astipanovic23@gmail.com>
2020-10-20 16:06:29 +0100

    The Standard Template Library: Two-Dimensional Vectors

---
+++ Learn Advanced C++ Programming/Two dimensional Vectors/Two dimensional Vectors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> Grid(3, vector<int>(4, 7));
+
+    Grid[1].push_back(8);
+    //Grid.push_back(vector<int>(1, 0));
+
+    for (int Row=0; Row < Grid.size(); Row++)
+    {
+        for (int Col = 0; Col < Grid[Row].size(); Col++)
+        {
+            cout << Grid[Row][Col] << flush;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
